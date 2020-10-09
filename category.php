c4fa5ff64a1b5fc237b6da799eba72a85ef17444
Lea Dalibard <leadalibard@gmail.com>
2020-10-09 16:00:59 +0200

    Adding a dropdown bootstrap

--- category.php
+++ category.php
@@ -100,6 +100,38 @@ echo $_SESSION["page"];
 <body>
 
 <div class="container">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle"
+                type="button" id="dropdownMenu1" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+            Dropdown
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <a class="dropdown-item" href="#!">Normal</a>
+            <a class="dropdown-item" href="#!">Fighting</a>
+            <a class="dropdown-item" href="#!">Flying</a>
+            <a class="dropdown-item" href="#!">Poison</a>
+            <a class="dropdown-item" href="#!">Ground</a>
+            <a class="dropdown-item" href="#!">Rock</a>
+            <a class="dropdown-item" href="#!">Bug</a>
+            <a class="dropdown-item" href="#!">Ghost</a>
+            <a class="dropdown-item" href="#!">Steel</a>
+            <a class="dropdown-item" href="#!">Fire</a>
+            <a class="dropdown-item" href="#!">Water</a>
+            <a class="dropdown-item" href="#!">Grass</a>
+            <a class="dropdown-item" href="#!">Electric</a>
+            <a class="dropdown-item" href="#!">Psychic</a>
+            <a class="dropdown-item" href="#!">Ice</a>
+            <a class="dropdown-item" href="#!">Dragon</a>
+            <a class="dropdown-item" href="#!">Dark</a>
+            <a class="dropdown-item" href="#!">Fairy</a>
+            <a class="dropdown-item" href="#!">Unknown</a>
+            <a class="dropdown-item" href="#!">Shadow</a>
+
+
+
+        </div>
+    </div>
     <nav aria-label="Page navigation">
         <ul class="pagination">
             <li class="page-item"><a class="page-link" href="?page=0">Previous</a></li>
