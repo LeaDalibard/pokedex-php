dea15a500a665bd4ea0d1b463e6e28b4d5791682
Lea Dalibard <leadalibard@gmail.com>
2020-10-09 11:46:34 +0200

    Setting
    href='?page=0' etc to get pages inside page navigation, now need to make a session variable to remember + change the pokemons called

--- category.php
+++ category.php
@@ -5,6 +5,11 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+
+//-------- START SESSION TO REMEMBER PAGES
+session_start();
+
+
 //-------- FUNCTION GET DATA
 
 function getData($poke)
@@ -38,8 +43,21 @@ function getImg($poke)
     return $imgPoke;
 }
 
+//-------- FUNCTION GET PAGE
+
 $page_length=21;
 
+if (isset ($_GET['page'])){
+    if ($_GET['page'] == 0){
+       echo "previous";
+    }
+    elseif ($_GET['page'] == 1){
+        echo "page 1";
+    }
+    elseif ($_GET['page'] == 2){
+        echo "page 2";
+    }
+}
 
 ?>
 
@@ -56,13 +74,24 @@ $page_length=21;
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+
 <div class="container">
+    <nav aria-label="Page navigation">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="?page=0">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="?page=1">1</a></li>
+            <li class="page-item"><a class="page-link" href="?page=2">2</a></li>
+            <li class="page-item"><a class="page-link" href="?page=3">3</a></li>
+            <li class="page-item"><a class="page-link" href="?page=4">Next</a></li>
+        </ul>
+    </nav>
     <div class="row">
         <?php for($i=1;$i<$page_length;$i++): ?>
         <div class="col-3">
             <p><?php echo "Pokemon name : " . ucwords(getName($i)); ?></p>
             <p><?php echo "Pokemon Id : " . getId($i); ?></p>
             <img src="<?php echo getImg($i); ?>" alt="pokemon image">
+            <p> <a href=<?php echo "https://pokeapi.co/api/v2/pokemon/".$i; ?>>Link to Pokemon page</a></p>
         </div>
         <?php endfor; ?>
     </div>
